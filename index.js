08cd0f883e0dfc554370cc4b3012abdd44696692
Rashmil333 <68959771+Rashmil333@users.noreply.github.com>
2021-07-06 00:20:33 +0530

    calculator by redux

---
+++ index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter} from "react-router-dom";
+import Dwebsite from "./Dwebsite";
+import Websocket from "./Websocket";
+import Dtestchat from "./Dtestchat";
+import Dreactreduxapp from "./Dreactreduxapp";
+import store from "./store";
+import {Provider} from "react-redux";
+import Dtodobyreactredux from "./Dtodobyreactredux";
+import Dreduxcalculator from "./Dreduxcalculator";
+
+store.subscribe(()=>console.log(store.getState()));
+	// <BrowserRouter>
+	// 	<Dwebsite/>
+	// </BrowserRouter>
+
+ReactDOM.render(<>
+	<Provider store={store}>
+		<Dreduxcalculator/>
+	</Provider>
+	</>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
